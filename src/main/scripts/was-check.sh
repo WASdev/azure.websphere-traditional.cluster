d20945a450f52cf9946757ac4f545919d754a42f
Jianguo Ma <jiangma@microsoft.com>
2021-09-26 13:24:32 +0800

    replace musl libc with glibc
    Signed-off-by: Jianguo Ma <jiangma@microsoft.com>

--- src/main/scripts/was-check.sh
+++ src/main/scripts/was-check.sh
@@ -14,27 +14,49 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+# Remove musl libc as IBM Java binaries only run on glibc
+apk del libc6-compat
+
+# Install glibc by referencing to https://github.com/ibmruntimes/ci.docker/blob/master/ibmjava/8/jre/alpine/Dockerfile
+apk add --no-cache --virtual .build-deps curl binutils \
+    && GLIBC_VER="2.30-r0" \
+    && ALPINE_GLIBC_REPO="https://github.com/sgerrand/alpine-pkg-glibc/releases/download" \
+    && GCC_LIBS_URL="https://archive.archlinux.org/packages/g/gcc-libs/gcc-libs-8.2.1%2B20180831-1-x86_64.pkg.tar.xz" \
+    && GCC_LIBS_SHA256=e4b39fb1f5957c5aab5c2ce0c46e03d30426f3b94b9992b009d417ff2d56af4d \
+    && curl -fLs https://alpine-pkgs.sgerrand.com/sgerrand.rsa.pub -o /tmp/sgerrand.rsa.pub \
+    && cp /tmp/sgerrand.rsa.pub /etc/apk/keys \
+    && curl -fLs ${ALPINE_GLIBC_REPO}/${GLIBC_VER}/glibc-${GLIBC_VER}.apk > /tmp/${GLIBC_VER}.apk \
+    && apk add /tmp/${GLIBC_VER}.apk \
+    && curl -fLs ${GCC_LIBS_URL} -o /tmp/gcc-libs.tar.xz \
+    && echo "${GCC_LIBS_SHA256}  /tmp/gcc-libs.tar.xz" | sha256sum -c - \
+    && mkdir /tmp/gcc \
+    && tar -xf /tmp/gcc-libs.tar.xz -C /tmp/gcc \
+    && mv /tmp/gcc/usr/lib/libgcc* /tmp/gcc/usr/lib/libstdc++* /usr/glibc-compat/lib \
+    && strip /usr/glibc-compat/lib/libgcc_s.so.* /usr/glibc-compat/lib/libstdc++.so* \
+    && apk del --purge .build-deps \
+    && apk add --no-cache ca-certificates openssl \
+    && rm -rf /tmp/${GLIBC_VER}.apk /tmp/gcc /tmp/gcc-libs.tar.xz /var/cache/apk/* /tmp/*.pub
+
 # Define const variables for IM installation
+IM_INSTALL_KIT_URL=https://public.dhe.ibm.com/ibmdl/export/pub/software/im/zips/agent.installer.linux.gtk.x86_64.zip
+IM_INSTALL_KIT=/tmp/agent.installer.linux.gtk.x86_64.zip
+IM_INSTALL_KIT_UNPACK=/tmp/im_installer
+IM_INSTALL_DIRECTORY=/tmp/IBM/InstallationManager/V1.9
+LOG_FILE=/tmp/deployment.log
 WAS_ND_VERSION_ENTITLED=ND.v90_9.0.5007
 NO_PACKAGES_FOUND="No packages were found"
-IM_INSTALL_KIT=agent.installer.linux.gtk.x86_64.zip
-IM_INSTALL_KIT_URL=https://public.dhe.ibm.com/ibmdl/export/pub/software/im/zips/${IM_INSTALL_KIT}
-IM_INSTALL_DIRECTORY=/tmp/IBM/InstallationManager/V1.9
-logFile=/tmp/deployment.log
 
-cd /tmp
-echo "$(date): Start to install IBM Installation Manager." > $logFile
+echo "$(date): Start to install IBM Installation Manager." > ${LOG_FILE}
 
 # Create installation directories
-mkdir -p ${IM_INSTALL_DIRECTORY}
+mkdir -p ${IM_INSTALL_KIT_UNPACK} && mkdir -p ${IM_INSTALL_DIRECTORY}
 
 # Install IBM Installation Manager
-wget -O "$IM_INSTALL_KIT" "$IM_INSTALL_KIT_URL" -q
-mkdir im_installer
-unzip -q "$IM_INSTALL_KIT" -d im_installer
-./im_installer/userinstc -log log_file -acceptLicense -installationDirectory ${IM_INSTALL_DIRECTORY}
+wget -O ${IM_INSTALL_KIT} ${IM_INSTALL_KIT_URL} -q
+unzip -q ${IM_INSTALL_KIT} -d ${IM_INSTALL_KIT_UNPACK}
+${IM_INSTALL_KIT_UNPACK}/userinstc -log ${LOG_FILE} -acceptLicense -installationDirectory ${IM_INSTALL_DIRECTORY}
 
-echo "$(date): IBM Installation Manager installed, start to check entitlement." >> $logFile
+echo "$(date): IBM Installation Manager installed, start to check entitlement." >> ${LOG_FILE}
 
 # Save credentials to a secure storage file
 ${IM_INSTALL_DIRECTORY}/eclipse/tools/imutilsc saveCredential -secureStorageFile storage_file \
@@ -42,26 +64,26 @@ ${IM_INSTALL_DIRECTORY}/eclipse/tools/imutilsc saveCredential -secureStorageFile
 
 # Check whether IBMid is entitled or not
 if [ $? -ne 0 ]; then
-    echo "Cannot connect to Passport Advantage while saving the credential to the secure storage file." >> $logFile
+    echo "Cannot connect to Passport Advantage while saving the credential to the secure storage file." >> ${LOG_FILE}
 fi
 
 result=0
 output=$(${IM_INSTALL_DIRECTORY}/eclipse/tools/imcl listAvailablePackages -cPA -secureStorageFile storage_file)
-if [ echo $output | grep -q "$WAS_ND_VERSION_ENTITLED" ]; then
-    echo "Entitled" >> $logFile
+if echo $output | grep -q "$WAS_ND_VERSION_ENTITLED"; then
+    echo "Entitled" >> ${LOG_FILE}
 else
     result=1
-    if [ echo $output | grep -q "$NO_PACKAGES_FOUND" ]; then
-        echo "Undefined" >> $logFile
+    if echo $output | grep -q "$NO_PACKAGES_FOUND"; then
+        echo "Undefined" >> ${LOG_FILE}
         echo "No WebSphere Application Server installation packages were found. This is likely due to a temporary issue with the installation repository. Try again and open an IBM Support issue if the problem persists."
     else
-        echo "Unentitled" >> $logFile
+        echo "Unentitled" >> ${LOG_FILE}
         echo "The provided IBM ID does not have entitlement to install WebSphere Application Server. Please contact the primary or secondary contacts for your IBM Passport Advantage site to grant you access or follow steps at IBM eCustomer Care (https://ibm.biz/IBMidEntitlement) for further assistance."
     fi
 fi
 
-# Remove temporary files
-rm -rf storage_file && rm -rf log_file
+# Remove the secure storage file
+rm -rf storage_file
 
-echo "$(date): Entitlement check completed." >> $logFile
+echo "$(date): Entitlement check completed." >> ${LOG_FILE}
 [ $result -eq 1 ] && exit 1
