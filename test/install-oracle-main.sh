4a54e0c021092c7c4ac93f43af4d9c312e0d051d
Jianguo Ma <jiangma@microsoft.com>
2022-10-20 14:49:43 +0800

    scripts to install oracle db on azure vm
    Signed-off-by: Jianguo Ma <jiangma@microsoft.com>

---
+++ test/install-oracle-main.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+#      Copyright (c) Microsoft Corporation.
+# 
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+# 
+#           http://www.apache.org/licenses/LICENSE-2.0
+# 
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+# Input parameters
+oracleDBPassword=$1
+
+# Check for last created disk device that we will format for use holding Oracle datafiles
+ls -alt /dev/sd*|head -1
+
+# Create a disk label
+parted /dev/sdc mklabel gpt
+
+# Create a primary partition spanning the whole disk
+parted -a optimal /dev/sdc mkpart primary 0GB 64GB
+
+# Check the device details by printing its metadata
+parted /dev/sdc print
+
+# Create a filesystem on the device partition
+mkfs -t ext4 /dev/sdc1
+
+# Create a mount point
+mkdir /u02
+
+# Mount the disk
+mount /dev/sdc1 /u02
+
+# Change permissions on the mount point
+chmod 777 /u02
+
+# Add the mount to the /etc/fstab file
+echo "/dev/sdc1               /u02                    ext4    defaults        0 0" >> /etc/fstab
+
+# Open firewall ports
+firewall-cmd --zone=public --add-port=1521/tcp --permanent
+firewall-cmd --zone=public --add-port=5502/tcp --permanent
+firewall-cmd --reload
+
+# Switch to the oracle user and install the database
+cp install-oracle.sh /tmp
+chmod 777 /tmp/install-oracle.sh
+runuser -l oracle -c "/tmp/install-oracle.sh ${oracleDBPassword}"
---
+++ test/install-oracle.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+#      Copyright (c) Microsoft Corporation.
+# 
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+# 
+#           http://www.apache.org/licenses/LICENSE-2.0
+# 
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+# Input parameters
+oracleDBPassword=$1
+
+# Start the database listener
+lsnrctl start
+
+# Create a data directory for the Oracle data files
+mkdir /u02/oradata
+
+# Run the Database Creation Assistant
+dbca -silent \
+   -createDatabase \
+   -templateName General_Purpose.dbc \
+   -gdbname oratest1 \
+   -sid oratest1 \
+   -responseFile NO_VALUE \
+   -characterSet AL32UTF8 \
+   -sysPassword ${oracleDBPassword} \
+   -systemPassword ${oracleDBPassword} \
+   -createAsContainerDatabase false \
+   -databaseType MULTIPURPOSE \
+   -automaticMemoryManagement false \
+   -storageType FS \
+   -datafileDestination "/u02/oradata/" \
+   -ignorePreReqs
+
+# Set Oracle variables
+export ORACLE_SID=oratest1
+echo "export ORACLE_SID=oratest1" >> ~oracle/.bashrc
+
+# Create test user and grant permissions
+sqlplus sys/${oracleDBPassword} as sysdba <<EOF
+create user testuser identified by ${oracleDBPassword};
+grant sysdba to testuser;
+grant create table, create view, create procedure, create sequence, create session to testuser;
+grant select on pending_trans$ to testuser;
+grant select on dba_2pc_pending to testuser;
+grant select on dba_pending_transactions to testuser;
+grant execute on dbms_xa to testuser;
+alter user testuser quota unlimited on users;
+EOF
